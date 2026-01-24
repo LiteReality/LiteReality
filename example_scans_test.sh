e7bcb69d1d53ffc407930999e9213fa25b541738
zheninghuang <huangzhening@gmail.com>
2026-01-24 11:55:55 +0000

    update bugs

--- example_scans_test.sh
+++ example_scans_test.sh
@@ -3,11 +3,11 @@
 set -e
 
 mappings=(
+    "2025_05_05_08_42_28 Darwin_BedRoom"
     "2024_11_13_21_42_41 Girton_large_study_room"
     "2025_01_20_08_44_07 BoardRoom_CUED"
     "2024_11_13_21_38_09 Girton_Study_Room"
     "2025_03_16_10_29_01 Girton_Common_Room"
-    "2025_05_05_08_42_28 Darwin_BedRoom"
     "2026_01_15_15_16_00 SigProc_meeting_room"
     "2024_11_29_13_53_13 Meeting_Room_CUED_Lab"
     "2025_03_14_18_54_12 SigProc_Tea_Room"
